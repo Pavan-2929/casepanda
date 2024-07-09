1d1b84679543c16cdcf843fb3014cd7adbdd219d
Pavan-2929 <kapdiyapavan3218@gmail.com>
2024-07-09 12:37:27 +0530

    fix email issue

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -54,7 +54,7 @@ const Navbar = () => {
               <ArrowRight />
             </Button>
           </Link>
-          {currentUser.email === "casepanda29@gmail.com" ? (
+          {currentUser && currentUser?.email === "casepanda29@gmail.com" ? (
             <Link
               href="/admin"
               className="text-primary px-3 rounded-md transition-all duration-200"
